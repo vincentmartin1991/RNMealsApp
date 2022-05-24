ba25c6a5a73e0c31cf2777b63edb68aafc03a3b7
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-05-23 18:33:57 -0700

    MealItem styling

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,17 +1,29 @@
-import { View, Pressable, Text, Image, StyleSheet } from "react-native";
+import {
+	View,
+	Pressable,
+	Text,
+	Image,
+	StyleSheet,
+	Platform,
+} from "react-native";
 
 function MealItem({ title, imageUrl, duration, complexity, affordability }) {
 	return (
-		<View>
-			<Pressable>
-				<View>
-					<Image source={{ uri: imageUrl }} style={styles.image} />
-					<Text style={styles.title}>{title}</Text>
-				</View>
-				<View>
-					<Text>{duration}</Text>
-					<Text>{complexity.toUpperCase()}</Text>
-					<Text>{affordability.toUpperCase()}</Text>
+		<View style={styles.mealItem}>
+			<Pressable
+				android_ripple={{ color: "#ccc" }}
+				style={({ pressed }) => (pressed ? styles.buttonPressed : null)}
+			>
+				<View style={styles.innerContainer}>
+					<View>
+						<Image source={{ uri: imageUrl }} style={styles.image} />
+						<Text style={styles.title}>{title}</Text>
+					</View>
+					<View style={styles.details}>
+						<Text style={styles.detailItem}>{duration}m</Text>
+						<Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+						<Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
+					</View>
 				</View>
 			</Pressable>
 		</View>
@@ -21,6 +33,24 @@ function MealItem({ title, imageUrl, duration, complexity, affordability }) {
 export default MealItem;
 
 const styles = StyleSheet.create({
+	mealItem: {
+		margin: 16,
+		borderRadius: 8,
+		backgroundColor: "white",
+		shadowColor: "black",
+		shadowOpacity: 0.25,
+		shadowOffset: { width: 0, height: 2 },
+		shadowRadius: 8,
+		overflow: Platform.OS === "android" ? "hidden" : "visible",
+	},
+	buttonPressed: {
+		opacity: 0.75,
+		backgroundColor: "#ccc",
+	},
+	innerContainer: {
+		borderRadius: 8,
+		overflow: "hidden",
+	},
 	image: {
 		width: "100%",
 		height: 200,
@@ -29,5 +59,16 @@ const styles = StyleSheet.create({
 		fontWeight: "bold",
 		textAlign: "center",
 		fontSize: 18,
+		margin: 8,
+	},
+	details: {
+		flexDirection: "row",
+		alignItems: "center",
+		padding: 8,
+		justifyContent: "center",
+	},
+	detailItem: {
+		marginHorizontal: 4,
+		fontSize: 12,
 	},
 });
