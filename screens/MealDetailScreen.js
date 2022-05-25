eef9b505440528ae99b5870c70825bb47f500c71
vincentmartin1991 <vincentmartin1991@gmail.com>
2022-05-25 11:04:01 -0700

    using useContext meals can now be marked as a favorite and saved to app-wide state

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import { useLayoutEffect } from "react";
+import { useLayoutEffect, useContext } from "react";
 import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 
 import IconButton from "../components/IconButton";
@@ -6,23 +6,37 @@ import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import { MEALS } from "../data/dummy-data";
+import { FavoritesContext } from "../store/context/favorites-context";
 
 function MealDetailScreen({ route, navigation }) {
+	const favoriteMealsCtx = useContext(FavoritesContext);
+
 	const mealId = route.params.mealId;
 
 	const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-	function headerButtonPressHandler() {
-		console.log("Pressed!");
+	mealIsFavorite = favoriteMealsCtx.ids.includes(mealId);
+
+	function changeFavoriteStatusHandler() {
+		if (mealIsFavorite) {
+			favoriteMealsCtx.removeFavorite(mealId);
+		} else {
+			favoriteMealsCtx.addFavorite(mealId);
+		}
 	}
 
 	useLayoutEffect(() => {
 		navigation.setOptions({
 			headerRight: () => {
-				return <IconButton icon="star" onPress={headerButtonPressHandler} />;
+				return (
+					<IconButton
+						icon={mealIsFavorite ? "star" : "star-outline"}
+						onPress={changeFavoriteStatusHandler}
+					/>
+				);
 			},
 		});
-	}, [navigation, headerButtonPressHandler]);
+	}, [navigation, changeFavoriteStatusHandler]);
 
 	return (
 		<ScrollView style={styles.rootContainer}>
